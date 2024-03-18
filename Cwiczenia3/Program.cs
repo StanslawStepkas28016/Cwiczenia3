eba1a7b9023b979ed359419acfb024090bda089a
Stanisław Stepka <s28016@pjwstk.edu.pl>
2024-03-18 09:38:11 +0100

    Zmiana struktury projektu

---
+++ Cwiczenia3/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Cwiczenia3;
+
+public abstract class Container
+{
+    public double GrossWeight { get; set; }
+    public double NetWeight { get; set; }
+    public double Height { get; set; }
+    public double Depth { get; set; }
+
+    protected Container(double grossWeight, double height, double netWeight, double depth)
+    {
+        GrossWeight = grossWeight;
+        Height = height;
+        NetWeight = netWeight;
+        Depth = depth;
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.Out.Write("asdasd");
+    }
+}
