038ac0274e41cbe365a3c58304532ca45acf4be4
Stanisław Stepka <s28016@pjwstk.edu.pl>
2024-03-18 09:58:24 +0100

    Zbudowanie klasy Container, do poprawy

--- Cwiczenia3/Program.cs
+++ Cwiczenia3/Program.cs
@@ -1,19 +1,29 @@
 ﻿namespace Cwiczenia3;
 
-abstract class ContainerObj
+public abstract class Container
 {
-    private double GrossWeight { get; set; }
-    private double NetWeight { get; set; }
+    private double GrossWeight { get; set; } // Waga kontener + towar.
+    private double NetWeight { get; set; } // Waga kontenera.
     private double Height { get; set; }
     private double Depth { get; set; }
     private string serialNum { get; set; }
 
-    protected ContainerObj(double grossWeight, double netWeight, double height, double depth)
+    protected Container(double grossWeight, double netWeight, double height, double depth)
     {
         GrossWeight = grossWeight;
         NetWeight = netWeight;
         Height = height;
         Depth = depth;
+    }
+
+    protected void emptyContainer()
+    {
+        GrossWeight -= NetWeight;
+    }
+
+    protected void loadContainer(double WeightToLoad)
+    {
+        GrossWeight = NetWeight + WeightToLoad;
     }
 }
 
